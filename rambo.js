a368eabfdeef9c279055e2cae99bd7e7898fc5e5
Krzysztof Golinski <kgolinski@pangenerator.com>
2014-06-28 15:49:18 +0200

    wypelnione dane rambo

--- rambo.js
+++ rambo.js
@@ -1,62 +1,65 @@
-var rambo = [
-    {
-        "name": "First Blood", //Name of the movie
-        "year": 1982, //Year of the premiere
-        "killsShirt": 1, //Number of bad guys killed by Rambo with his shirt on
-        "killsNoShirt": 0, //Number of bad guys killed by Rambo with his shirt off
-        "kills": 1, //Number of bad guys killed by Rambo no matter how attired
-        "accomplices": 0, //Number of bad guys killed by accomplices of Rambo acting on their own
-        "goodByBad": 0, //Number of good guys killed by bad guys
-        "total": 1, //Total number of people killed
-        "perMinute": 0.01, //Number of people killed per minute
-        "first": 29 * 60 + 31, //Time at which the first person is killed (seconds)
-        "perMinuteAfter": 0.02, //Number of people killed per minute after that point until the end of the film (not including the ending credits) 
-        "noResult": 12, //Sequences in which Rambo is shot without significant result
-        "torture": 2, //Number of sequences in which good guys are tortured by bad guys
-        "sexScenes": 0
-}, {
-        "name": "Rambo: First Blood Part II", //Name of the movie
-        "year": 1985, //Year of the premiere
-        "killsShirt": 12, //Number of bad guys killed by Rambo with his shirt on
-        "killsNoShirt": 46, //Number of bad guys killed by Rambo with his shirt off
-        "kills": 58, //Number of bad guys killed by Rambo no matter how attired
-        "accomplices": 10, //Number of bad guys killed by accomplices of Rambo acting on their own
-        "goodByBad": 1, //Number of good guys killed by bad guys
-        "total": 69, //Total number of people killed
-        "perMinute": 0.72, //Number of people killed per minute
-        "first": , //Time at which the first person is killed (seconds)
-        "perMinuteAfter": , //Number of people killed per minute after that point until the end of the film (not including the ending credits) 
-        "noResult": , //Sequences in which Rambo is shot without significant result
-        "torture": , //Number of sequences in which good guys are tortured by bad guys
-        "sexScenes": 0
-}, {
-        "name": "Rambo III", //Name of the movie
-        "year": 1988, //Year of the premiere
-        "killsShirt": , //Number of bad guys killed by Rambo with his shirt on
-        "killsNoShirt": , //Number of bad guys killed by Rambo with his shirt off
-        "kills": , //Number of bad guys killed by Rambo no matter how attired
-        "accomplices": , //Number of bad guys killed by accomplices of Rambo acting on their own
-        "goodByBad": , //Number of good guys killed by bad guys
-        "total": , //Total number of people killed
-        "perMinute": , //Number of people killed per minute
-        "first": , //Time at which the first person is killed (seconds)
-        "perMinuteAfter": , //Number of people killed per minute after that point until the end of the film (not including the ending credits) 
-        "noResult": , //Sequences in which Rambo is shot without significant result
-        "torture": , //Number of sequences in which good guys are tortured by bad guys
-        "sexScenes": 0
-}, {
-        "name": "Rambo", //Name of the movie
-        "year": 2008 //Year of the premiere
-        "killsShirt": , //Number of bad guys killed by Rambo with his shirt on
-        "killsNoShirt": , //Number of bad guys killed by Rambo with his shirt off
-        "kills": , //Number of bad guys killed by Rambo no matter how attired
-        "accomplices": , //Number of bad guys killed by accomplices of Rambo acting on their own
-        "goodByBad": , //Number of good guys killed by bad guys
-        "total": , //Total number of people killed
-        "perMinute": , //Number of people killed per minute
-        "first": , //Time at which the first person is killed (seconds)
-        "perMinuteAfter": , //Number of people killed per minute after that point until the end of the film (not including the ending credits) 
-        "noResult": , //Sequences in which Rambo is shot without significant result
-        "torture": , //Number of sequences in which good guys are tortured by bad guys
-        "sexScenes": 0
+var rambo =
+[{
+    "name": "First Blood", //Name of the movie
+    "year": 1982, //Year of the premiere
+    "killsShirt": 1, //Number of bad guys killed by Rambo with his shirt on
+    "killsNoShirt": 0, //Number of bad guys killed by Rambo with his shirt off
+    "kills": 1, //Number of bad guys killed by Rambo no matter how attired
+    "accomplices": 0, //Number of bad guys killed by accomplices of Rambo acting on their own
+    "goodByBad": 0, //Number of good guys killed by bad guys
+    "total": 1, //Total number of people killed
+    "perMinute": 0.01, //Number of people killed per minute
+    "first": 29 * 60 + 31, //Time at which the first person is killed (seconds)
+    "perMinuteAfter": 0.02, //Number of people killed per minute after that point until the end of the film (not including the ending credits) 
+    "noResult": 12, //Sequences in which Rambo is shot without significant result
+    "torture": 2, //Number of sequences in which good guys are tortured by bad guys
+    "sexScenes": 0
+},
+{
+    "name": "Rambo: First Blood Part II", //Name of the movie
+    "year": 1985, //Year of the premiere
+    "killsShirt": 12, //Number of bad guys killed by Rambo with his shirt on
+    "killsNoShirt": 46, //Number of bad guys killed by Rambo with his shirt off
+    "kills": 58, //Number of bad guys killed by Rambo no matter how attired
+    "accomplices": 10, //Number of bad guys killed by accomplices of Rambo acting on their own
+    "goodByBad": 1, //Number of good guys killed by bad guys
+    "total": 69, //Total number of people killed
+    "perMinute": 0.72, //Number of people killed per minute
+    "first": 33 * 60 + 34, //Time at which the first person is killed (seconds)
+    "perMinuteAfter": 1.18, //Number of people killed per minute after that point until the end of the film (not including the ending credits) 
+    "noResult": 24, //Sequences in which Rambo is shot without significant result
+    "torture": 5, //Number of sequences in which good guys are tortured by bad guys
+    "sexScenes": 0
+},
+{
+    "name": "Rambo III", //Name of the movie
+    "year": 1988, //Year of the premiere
+    "killsShirt": 33, //Number of bad guys killed by Rambo with his shirt on
+    "killsNoShirt": 45, //Number of bad guys killed by Rambo with his shirt off
+    "kills": 78, //Number of bad guys killed by Rambo no matter how attired
+    "accomplices": 17, //Number of bad guys killed by accomplices of Rambo acting on their own
+    "goodByBad": 37, //Number of good guys killed by bad guys
+    "total": 132, //Total number of people killed
+    "perMinute": 1.30, //Number of people killed per minute
+    "first": 41 * 60 + 9, //Time at which the first person is killed (seconds)
+    "perMinuteAfter": 2.39, //Number of people killed per minute after that point until the end of the film (not including the ending credits) 
+    "noResult": 38, //Sequences in which Rambo is shot without significant result
+    "torture": 7, //Number of sequences in which good guys are tortured by bad guys
+    "sexScenes": 0
+},
+{
+    "name": "Rambo", //Name of the movie
+    "year": 2008 //Year of the premiere
+    "killsShirt": 83, //Number of bad guys killed by Rambo with his shirt on
+    "killsNoShirt": 0, //Number of bad guys killed by Rambo with his shirt off
+    "kills": 83, //Number of bad guys killed by Rambo no matter how attired
+    "accomplices": 40, //Number of bad guys killed by accomplices of Rambo acting on their own
+    "goodByBad": 113, //Number of good guys killed by bad guys
+    "total": 236, //Total number of people killed
+    "perMinute": 2.59, //Number of people killed per minute
+    "first": 3 * 60 + 22, //Time at which the first person is killed (seconds)
+    "perMinuteAfter": 3.04, //Number of people killed per minute after that point until the end of the film (not including the ending credits) 
+    "noResult": 2, //Sequences in which Rambo is shot without significant result
+    "torture": 3, //Number of sequences in which good guys are tortured by bad guys
+    "sexScenes": 0
 }];
